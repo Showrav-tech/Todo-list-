f1c78a61ee06ce898bf353c8c9af6180e074f622
Showrav <showravc85@gmail.com>
2026-03-07 23:28:12 +0600

    Done the Todo-list just need Deployment

--- script.js
+++ script.js
@@ -1,24 +1,44 @@
-const inputBox=document.getElementById("input-box");
-const listContainer=document.getElementById("list-container");
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
 
-function addTask(){
-   if(inputBox.value===''){
-    alert("You must write something!");
-   } 
-   else{
-    let li=document.createElement("li");
-    li.innerHTML=inputBox.value;
-    listContainer.appendChild(li);
-    let span=document.createElement("span");
-    span.innerHTML="\u00d7"
-    li .appendChild(span);
-   }
-   inputBox.value="";
-}
-listContainer.addEventListener("click",function(e){
-    if(e.target.tagName === "LI"){
-  e.target.classList.toggle("checked");
-    } else if(e.target.tagName === "SPAN"){
-        e.target.parentElement.remove();
+function addTask() {
+    if (inputBox.value.trim() === '') {
+        alert("You must write something!");
+    } else {
+        let li = document.createElement("li");
+        li.textContent = inputBox.value;
+
+        let span = document.createElement("span");
+        span.textContent = "\u00d7";
+        li.appendChild(span);
+
+        listContainer.appendChild(li);
+        inputBox.value = "";
+        saveData();
     }
-},false );
+}
+
+listContainer.addEventListener("click", function(e) {
+    if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData();
+    } else if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+});
+
+inputBox.addEventListener("keypress", function(e) {
+    if (e.key === "Enter") addTask();
+});
+
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showList() {
+    const data = localStorage.getItem("data");
+    if (data) listContainer.innerHTML = data;
+}
+
+showList();
